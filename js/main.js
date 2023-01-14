e3aa5527d6981fb5c9d9f329d430ef44b18e77b0
Misha Tyshkul <tyshkulm@gmail.com>
2023-01-14 09:49:22 +0200

    fixed function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-const Myinput = document.getElementById("text");
-
 function handleClick(event) {
-  let linkArr = Myinput.value.split("");
-  let arrText = [];
-  for (let i = 0; i < 5; i++) {
-    arrText.push(linkArr[i]);
+  const myInput = document.getElementById("text").value;
+  let site = "";
+  if (!(myInput.includes(`http`) || myInput.includes(`https`))) {
+    site = "https://" + myInput;
   }
-  for (let i = 0; i < arrText.length; i++) {
-    if (arrText[i] === [`http`] || arrText[i] === [`https`]) {
-      continue;
-    } else {
-      linkArr.unshift(`https://`);
-      break;
-    }
-  }
-  let stringLink = linkArr.join("");
-
-  location.href = stringLink;
+  location.href = site;
 }
 
 document.getElementById("first-btn").addEventListener("click", handleClick);
